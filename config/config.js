161647962da13894263202c62175610e858f524f
bigthyme <asinghavong@gmail.com>
2013-10-19 15:05:45 -0700

    incorporating headless login to dropbox

---
+++ config/config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.db_api_key = 'oqytb4375gdk3nn';
+config.db_api_secret = 'dd7wbk2nf2se7xn';
+
+module.exports = config;
--- config/config.rb
+++
@@ -1,2 +0,0 @@
-db_api_key = 'oqytb4375gdk3nn'
-db_api_secret = 'dd7wbk2nf2se7xn'
